ab91e1b8cb377f67b7f1468f4d04794f0bfb6a9c
mk2312 <al221910421@gmail.com>
2025-07-12 11:46:44 -0600

    feat: Agregar integración de sitemap en la configuración de Astro y actualizar dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), sitemap()]
 });
